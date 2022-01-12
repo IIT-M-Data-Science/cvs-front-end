888f74d89ce992828cebe462d8656fbc04ee5b58
Noufal Rahman <50796694+Rahman24@users.noreply.github.com>
2022-01-12 20:42:04 +0530

    Add files via upload

---
+++ script/verify.js
@@ -0,0 +1,5 @@
+function verify() {
+  document.querySelector(".mask").style.display="flex";
+  document.querySelector(".cert").style.display="block";
+  document.querySelector(".cert-x").style.display="block";
+}
