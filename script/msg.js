eb996866e0b07041236e66ab1eee50c329519ccb
Noufal Rahman <50796694+Rahman24@users.noreply.github.com>
2022-01-13 17:03:04 +0530

    Add files via upload

--- script/msg.js
+++ script/msg.js
@@ -1,5 +1,5 @@
 function showMsg(type, msg) {
-  var renderedMsg = "<h1 class='msg-heading'>"+type+"</h1><p class='msg'>"+msg+"</p><div class='btn' onclick='back()'>OK</div>";
+  var renderedMsg = "<h1 class='msg-heading'>"+type+"</h1><p class='msg'>"+msg+"</p><div class='btn-placeholder'><div class='btn' onclick='back()'>OK</div></div>";
   document.querySelector(".mask").style.display="flex";
   document.querySelector(".msg").style.display = "block";
   document.querySelector(".msg").innerHTML = renderedMsg;
