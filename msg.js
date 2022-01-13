d243357ce715b1f337e38fae007db10c37683e0d
Noufal Rahman <50796694+Rahman24@users.noreply.github.com>
2022-01-13 17:13:54 +0530

    Delete msg.js

--- msg.js
+++
@@ -1,9 +0,0 @@
-function showMsg(type, msg) {
-  document.querySelector(".mask").style.display="flex";
-  document.querySelector(".msg-heading").innerHTML=type;
-  document.querySelector(".msg").innerHTML=msg;
-}
-
-function back() {
-  document.querySelector(".mask").style.display="none";
-}
